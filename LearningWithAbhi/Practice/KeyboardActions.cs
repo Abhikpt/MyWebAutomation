eb5ef835760ef6f1573225f2dcf93e0b233c8a91
AbhishekTest <abhishek@gmail.com>
2025-01-29 11:46:12 +0530

    Keyword boartd

--- LearningWithAbhi/Practice/KeyboardActions.cs
+++ LearningWithAbhi/Practice/KeyboardActions.cs
@@ -1,9 +1,12 @@
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V123.IndexedDB;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
-public class KeyboardAction
+namespace LearningWithAbhi.Practice;
+public class KeyboardAction : IDisposable
 {
 
      public IWebDriver driver;
@@ -16,10 +19,48 @@ public class KeyboardAction
                     
     }
 
+   
 
+    [Test, Category("keyboard actions")]   
     public void PassUpperCase()
     {
-           ;
+       var elm1 =  driver.FindElement(By.Id("search"));
+       elm1.SendKeys(Keys.Shift + "debian");
 
+       elm1.SendKeys(Keys.Control + "A");
+        Thread.Sleep(2000);
+       elm1.SendKeys(Keys.Backspace);
+       Thread.Sleep(2000);
+     //  Console.ReadKey(); 
+
+        elm1.SendKeys(Keys.Shift + "Inserted with Shift") ;       
+
+    }
+
+    [Test, Category("Keyboard with Action class")]
+    public void KeyWordActionUsingActionCls()
+    {
+        Actions actions = new Actions(driver);
+
+        // Press and release a key
+        var elm1 =  driver.FindElement(By.Id("search"));
+        elm1.SendKeys("Working with Action class");
+
+        var search = driver.FindElement(By.CssSelector("button[ title='Search']"));
+        actions.SendKeys(search,Keys.Enter).Perform();
+        Thread.Sleep(2000);
+
+        elm1 =  driver.FindElement(By.Id("search"));  // will get stale element error not locating again
+        //press the Control key don and send A to perform Select All
+        actions.SendKeys(elm1," ").KeyDown(Keys.Control).SendKeys("a" + Keys.Backspace).Perform();
+        Thread.Sleep(5000);
+       
+    }
+
+
+
+     public void Dispose()
+    {
+        driver.Dispose();
     }
 }
