e35d95b54c1e798f106664ed1e8d128f3ff1b9a0
AbhishekTest <abhishek@gmail.com>
2025-03-26 13:32:41 +0530

    basic upgrades

--- FrameWorkDesign/obj/Debug/net8.0/FrameWorkDesign.AssemblyInfo.cs
+++ FrameWorkDesign/obj/Debug/net8.0/FrameWorkDesign.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrameWorkDesign")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63d7f20fb869bb1c3531d5de57a155afc724378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf36fa9d05e5b42dcfd975e29ca154aa9151ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrameWorkDesign")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrameWorkDesign")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
